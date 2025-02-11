27f0b1affb28710d11204d9cf445255bf967ad1f
haz2107 <haz2107@gmail.com>
2025-02-11 03:33:45 +0300

    Update Amiklampa.js

--- Amiklampa.js
+++ Amiklampa.js
@@ -6,10 +6,11 @@
   var pluginVersion = '1.0';
 
   // Основной URL API для нового источника
-  var apiUrl = 'https://github.com/haz2107/jsamik/Amiklampa.js'; // Замените на реальный URL API
+  var apiUrl = 'https://api.example.com/search'; // Замените на реальный URL API
+
   // Функция для загрузки данных с API
   function fetchData(url, callback) {
-    Lampa.Utils.fetch(url)
+    fetch(url)
       .then(function(response) {
         return response.json();
       })
@@ -34,7 +35,7 @@
     };
 
     this.fetchContent = function() {
-      var url = apiUrl + '/search?q=' + encodeURIComponent(object.movie.title);
+      var url = apiUrl + '?q=' + encodeURIComponent(object.movie.title);
       fetchData(url, function(data) {
         if (data && data.results) {
           this.displayContent(data.results);
@@ -85,55 +86,10 @@
 
   // Регистрация плагина
   function startPlugin() {
-    Lampa.Manifest.plugins = {
+    Lampa.Manifest.plugins.push({
       type: 'video',
       version: pluginVersion,
       name: pluginName,
       description: 'Плагин для просмотра онлайн контента от Amik',
       component: 'amik',
-      onContextMenu: function(object) {
-        return {
-          name: 'Смотреть онлайн (Amik)',
-          description: 'Плагин для просмотра онлайн контента от Amik'
-        };
-      },
-      onContextLauch: function(object) {
-        Lampa.Component.add('amik', AmikComponent);
-        Lampa.Activity.push({
-          url: '',
-          title: 'Онлайн (Amik)',
-          component: 'amik',
-          movie: object
-        });
-      }
-    };
-
-    // Добавление кнопки в интерфейс
-    Lampa.Listener.follow('full', function(e) {
-      if (e.type == 'complite') {
-        var btn = $('<div class="full-start__button selector view--online amik--button">\
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">\
-            <path d="M0 0h24v24H0z" fill="none"/>\
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"/>\
-          </svg>\
-          <span>Amik</span>\
-        </div>');
-        btn.on('hover:enter', function() {
-          Lampa.Activity.push({
-            url: '',
-            title: 'Онлайн (Amik)',
-            component: 'amik',
-            movie: e.data.movie
-          });
-        });
-        e.object.activity.render().find('.view--torrent').after(btn);
-      }
-    });
-  }
-
-  // Запуск плагина
-  if (!window.amikPlugin) {
-    window.amikPlugin = true;
-    startPlugin();
-  }
-})();
+      onContextMenu: function(object)
