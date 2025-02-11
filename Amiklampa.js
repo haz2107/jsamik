79ae60364bc8392ff040231fad78e30607fc3657
haz2107 <haz2107@gmail.com>
2025-02-11 03:45:35 +0300

    Update Amiklampa.js

--- Amiklampa.js
+++ Amiklampa.js
@@ -1,95 +1,38 @@
 (function() {
-  'use strict';
+         // Название плагина
+         const pluginName = "CinemaPlugin";
 
-  // Название плагина и его версия
-  var pluginName = 'AmikLampa';
-  var pluginVersion = '1.0';
+         // Регистрация плагина в Lampa
+         if (window.lampa && window.lampa.plugins) {
+             window.lampa.plugins.register(pluginName, function() {
+                 // Логика плагина
+                 this.init = function() {
+                     console.log("Cinema Plugin initialized!");
+                     // Добавьте здесь вызовы API Cinema и интеграцию с Lampa
+                 };
 
-  // Основной URL API для нового источника
-  var apiUrl = 'https://api.example.com/search'; // Замените на реальный URL API
+                 // Метод для поиска контента
+                 this.search = function(query) {
+                     // Логика поиска через Cinema API
+                     return fetch(`https://api.cinema.example/search?q=${query}`)
+                         .then(response => response.json())
+                         .then(data => {
+                             // Обработка данных и возврат в формате, понятном Lampa
+                             return data.results.map(item => ({
+                                 title: item.title,
+                                 description: item.description,
+                                 image: item.poster,
+                                 link: item.url
+                             }));
+                         });
+                 };
 
-  // Функция для загрузки данных с API
-  function fetchData(url, callback) {
-    fetch(url)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        callback(data);
-      })
-      .catch(function(error) {
-        console.error('Ошибка при загрузке данных:', error);
-      });
-  }
-
-  // Компонент для отображения контента
-  function AmikComponent(object) {
-    var network = new Lampa.Reguest();
-    var scroll = new Lampa.Scroll({ mask: true, over: true });
-    var files = new Lampa.Explorer(object);
-    var filter = new Lampa.Filter(object);
-
-    this.initialize = function() {
-      this.loading(true);
-      this.fetchContent();
-    };
-
-    this.fetchContent = function() {
-      var url = apiUrl + '?q=' + encodeURIComponent(object.movie.title);
-      fetchData(url, function(data) {
-        if (data && data.results) {
-          this.displayContent(data.results);
-        } else {
-          this.noContent();
-        }
-      }.bind(this));
-    };
-
-    this.displayContent = function(items) {
-      scroll.clear();
-      items.forEach(function(item) {
-        var html = Lampa.Template.get('online_item', {
-          title: item.title,
-          poster: item.poster,
-          year: item.year,
-          description: item.description
-        });
-        html.on('hover:enter', function() {
-          Lampa.Player.play({
-            title: item.title,
-            url: item.url,
-            quality: item.quality,
-            subtitles: item.subtitles
-          });
-        });
-        scroll.append(html);
-      });
-      this.loading(false);
-    };
-
-    this.noContent = function() {
-      scroll.clear();
-      scroll.append(Lampa.Template.get('online_empty', {
-        message: 'Контент не найден'
-      }));
-      this.loading(false);
-    };
-
-    this.loading = function(status) {
-      if (status) {
-        scroll.body().append(Lampa.Template.get('online_loading'));
-      } else {
-        scroll.body().find('.online-loading').remove();
-      }
-    };
-  }
-
-  // Регистрация плагина
-  function startPlugin() {
-    Lampa.Manifest.plugins.push({
-      type: 'video',
-      version: pluginVersion,
-      name: pluginName,
-      description: 'Плагин для просмотра онлайн контента от Amik',
-      component: 'amik',
-      onContextMenu: function(object)
+                 // Метод для воспроизведения контента
+                 this.play = function(url) {
+                     // Логика для воспроизведения видео через Cinema
+                     console.log(`Playing content from Cinema: ${url}`);
+                     // Интеграция с видеоплеером Lampa
+                 };
+             });
+         }
+     })();
